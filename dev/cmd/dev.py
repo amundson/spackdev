6f56d0d1fcf16e530c4d0fe364db59a05eef6164
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2019-11-01 21:38:40 +0100

    Import subcommands automatically without referring to them by name

--- dev/cmd/dev.py
+++ dev/cmd/dev.py
@@ -1,9 +1,23 @@
+import glob
+import os.path
 import spack
 import spack.cmd
 
 # Import sub-commands
-from ..subcommands import build_env, findext, getdeps, info, init, stage
-subcommands = ['build_env', 'findext', 'getdeps', 'info', 'init', 'stage']
+subcommands = []
+
+
+def import_subcommands_automatically():
+    subcommand_glob = os.path.join(os.path.dirname(__file__), '..', 'subcommands', '*.py')
+    command_files = glob.glob(subcommand_glob)
+    subcommands.extend(filter(
+        lambda x: not x.startswith('__'), [os.path.basename(x)[:-3] for x in command_files]
+    ))
+    t = __import__('subcommands', globals(), locals(), subcommands, 2)
+    for name in subcommands:
+        globals()[name] = getattr(t, name)
+
+import_subcommands_automatically()
 
 description = 'develop multiple Spack packages simultaneously'
 section = 'developer'
